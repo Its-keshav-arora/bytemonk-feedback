d330b8719ebbe4e367125c4b7f0ff25eec24a989
keshav arora <keshav11y@gmail.com>
2025-08-28 14:22:25 +0530

    updated cloud db credentials

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.PG_ADMIN_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 export default pool;
