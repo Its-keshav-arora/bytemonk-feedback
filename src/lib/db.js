a13a873e592da7b329e467ee5a8013db048e71d7
keshav arora <keshav11y@gmail.com>
2025-08-29 15:14:27 +0530

    updated cloud db credentials

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:postgres@localhost:5432/mydb",
+  connectionString: "postgres://bytemonk:bytemonk@bytemonk-7btl3-postgresql.bytemonk-7btl3.svc.cluster.local:5432/bytemonk",
 });
 
 export default pool;
