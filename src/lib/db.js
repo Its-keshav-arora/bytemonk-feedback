7d924e5d7055705d296051ba687d020b468104a6
keshav arora <keshav11y@gmail.com>
2025-08-29 15:26:54 +0530

    fixed cloud db connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgres://bytemonk:bytemonk@bytemonk-7btl3-postgresql.bytemonk-7btl3.svc.cluster.local:5432/bytemonk",
+  connectionString: "postgres://bytemonk:bytemonk@asia-east1-001.proxy.kinsta.app:30354/bytemonk",
 });
 
 export default pool;
